0ac5737d5698834a8e6cdb2a03697c435b9f60ba
shitanshu-277 <shitanshugoyal2002@gmail.com>
2025-10-05 12:29:58 +0000

    implemented infinite scroll

--- src/components/body.js
+++ src/components/body.js
@@ -3,18 +3,31 @@ import Card from './card'
 import Shimmer from './shimmer'
 
 const Body = () =>{
-    const [memes,setMemes] = useState(null);
+    const [memes,setMemes] = useState([]);
+    const [showShimmer,setShowShimmer] = useState(false);
     useEffect(()=>{
-        fetchMemes()
+        fetchMemes();
+        window.addEventListener('scroll',handleScroll);
+        return ()=> window.removeEventListener('scroll',handleScroll);
     },[])
+
+    const handleScroll = () => {
+        if(window.innerHeight + window.scrollY >= document.body.scrollHeight-1){
+            fetchMemes();
+        }
+    }
+
     const fetchMemes = async () =>{
+        setShowShimmer(true);
         const data = await fetch('https://meme-api.com/gimme/20');
         const json = await data.json()
-        setMemes(json.memes);
+        setShowShimmer(false);
+        setMemes((memes) => [...memes,...json.memes]);
         console.log(json);
     }
     return <div className="flex flex-wrap">
-        {!memes ? <Shimmer/> : memes.map((meme,i)=> <Card key={i} data={meme}/>)}
+        {memes.map((meme,i)=> <Card key={i} data={meme}/>)}
+        {showShimmer && <Shimmer/>}
     </div>
 }
 export default Body
--- src/components/card.js
+++ src/components/card.js
@@ -3,7 +3,7 @@ const card = ({data}) =>{
     return <div className="p-5 m-5 h-64 w-64 border border-black rounded-lg">
         <p>{title}</p>
         <p>{author}</p>
-        <img alt="meme" src={url}></img>
+        <img alt="meme" src={url} className="w-full h-40 object-cover rounded-md mt-2"></img>
     </div>
 }
 
